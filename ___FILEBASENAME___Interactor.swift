3c499b029b13fe29f599b9471f064ef0cb4b9371
caflatlander <caflatlander@users.noreply.github.com>
2025-06-03 08:00:48 -0400

    Delete ___FILEBASENAME___Interactor.swift
    i b idiot

--- ___FILEBASENAME___Interactor.swift
+++
@@ -1,8 +0,0 @@
-import SwiftUI
-
-@MainActor
-protocol ___VARIABLE_productName:identifier___Interactor {
-    func trackEvent(event: LoggableEvent)
-}
-
-extension ___VARIABLE_coreName:identifier___Interactor: ___VARIABLE_productName:identifier___Interactor { }
