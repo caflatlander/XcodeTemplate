5b9fe8f283c16227d44b1bcc91d3d6db095d0cc1
caflatlander <caflatlander@users.noreply.github.com>
2025-06-03 07:59:20 -0400

    Delete VIPERTemplate.xctemplate directory
    i b idiot

--- VIPERTemplate.xctemplate/___FILEBASENAME___/___FILEBASENAME___Router.swift
+++
@@ -1,8 +0,0 @@
-import SwiftUI
-
-@MainActor
-protocol ___VARIABLE_productName:identifier___Router {
-    
-}
-
-extension ___VARIABLE_coreName:identifier___Router: ___VARIABLE_productName:identifier___Router { }
